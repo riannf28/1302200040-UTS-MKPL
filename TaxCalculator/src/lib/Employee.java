e8975149edf937a4aadab7424d7676a2af89cf3c
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2023-04-14 18:06:35 +0700

    Mengubah tipe data gender
    Mengubah tipe data gender karena boolean bukan tipe data yang sesuai. Saya menggunakan enumeration (enum)

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -5,6 +5,11 @@ import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
+public enum Gender {
+	MALE,
+	FEMALE
+}
+
 public class Employee {
 
 	private String employeeId;
@@ -19,7 +24,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Gender gender;
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,7 +36,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
