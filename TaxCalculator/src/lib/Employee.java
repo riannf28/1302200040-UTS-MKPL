083d1e04697db8502ae7d52d32fddbe7df4b8633
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2023-04-14 23:50:58 +0700

    Membuat Class Spouse
    Membuat Class baru bernama Spouse, berisikan data spouse yang ada pada class Employee

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -11,7 +11,7 @@ public enum Gender {
 	FEMALE
 }
 
-public class Employee {
+public class Employee extends Spouse {
 
 	private String employeeId;
 	private String firstName;
@@ -29,9 +29,6 @@ public class Employee {
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
@@ -49,11 +46,6 @@ public class Employee {
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
 	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
@@ -80,11 +72,6 @@ public class Employee {
 		this.otherMonthlyIncome = income;
 	}
 	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
-	}
-	
 	public void addChild(String childName, String childIdNumber) {
 		childNames.add(childName);
 		childIdNumbers.add(childIdNumber);
@@ -92,7 +79,6 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == joinedDate.year) {
---
+++ TaxCalculator/src/lib/Spouse.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class Spouse {
+    String spouseName;
+	String spouseIdNumber;
+
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+		this.spouseName = spouseName;
+		this.spouseIdNumber = idNumber;
+	}
+}
+
