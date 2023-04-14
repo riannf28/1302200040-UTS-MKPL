c2147cae41fe5aafac9d0a987708fe136cf802a7
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2023-04-14 23:37:22 +0700

    Menghapus comment
    Menghapus comment pada setMonthSalary dan getAnnualIncomeTax karena menjelaskan logika yang ada pada method. Karena code yang ada sudah dapat dimengerti tanpa adanya comment pun.

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -50,11 +50,6 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
@@ -92,7 +87,6 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == joinedDate.year) {
